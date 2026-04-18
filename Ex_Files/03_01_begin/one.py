e97cdb58b060b81c030f79001357f93077ffd993
Ruchita Hambir <82107508+ruchitahambir@users.noreply.github.com>
2026-04-18 18:18:29 +0000

    added few more practice codes for csv file moon.

--- Ex_Files/03_01_begin/one.py
+++ Ex_Files/03_01_begin/one.py
@@ -1,7 +1,23 @@
 import csv
-
+# Open and read your new file
 with open('moon.csv','r') as a:
   reader = csv.DictReader(a)
   moon = list(reader)
-
 print (f"---Loaded {len(moon)} space Moon mission ---\n")
+
+# Practice 1: Print all mission names
+print("All Mission Names:")
+for a in moon:
+    print(f" - {a['mission_name']}")
+
+# Practice 2: Filter for missions to the 'Moon'
+print("\nMoon Missions:")
+for m in moon:
+    if m['destination'] == 'Moon':
+        print(f" > {m['mission_name']} was launched in {m['year']}")
+
+# Practice 3: Check for failures
+print("\nFailed Missions:")
+for m in moon:
+    if m['status'] == 'Failure':
+        print(f" ! Alert: {m['mission_name']} to {m['destination']} failed.")
